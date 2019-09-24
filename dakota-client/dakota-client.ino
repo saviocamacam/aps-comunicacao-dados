2ede72c7c08bf237960d8c57cba124fb72f7e7a2
Savio <saviocamacam@gmail.com>
2019-09-24 14:32:28 -0300

    Separação em arquivos de cliente e servidor

---
+++ dakota-client/dakota-client.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include "RF24.h"
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+
+void dtcp() {
+  Serial.println(F("RF24/Dakota-client"));
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(F("RF24/Dakota-client"));
+  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+ 
+  // Setup and configure radio
+
+  radio.begin();
+  radio.startListening();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
