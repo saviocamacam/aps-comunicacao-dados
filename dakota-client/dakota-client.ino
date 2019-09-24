e1ace2ed977534e7a8a841666bbcf368ca545a29
Savio <saviocamacam@gmail.com>
2019-09-24 15:16:28 -0300

    Adicionada lógica inicial de comunicação

--- dakota-client/dakota-client.ino
+++ dakota-client/dakota-client.ino
@@ -1,26 +1,60 @@
 #include <SPI.h>
 #include "RF24.h"
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
+/* CONFIGURAÇÃO DE HARDWARE: CONFIGURA O RÁDIO nRF24L01 EM BARRAMENTO SPI COM PINOS 7 & 8 */
+RF24 radio(7, 8);
+// DEFININDO OS MODOS QUE O NÓ PODE ASSUMIR
+typedef enum
+{
+  transmitting = 1,
+  listening
+} mode;
 
-void dtcp() {
+mode currentMode = listening;
+const char *myAddress;
+
+//FUNÇÃO QUE VERIFICA SE JÁ TENHO ENDEREÇO. SE NÃO, EU PEÇO;
+void dtcp()
+{
   Serial.println(F("RF24/Dakota-client"));
 }
 
-void setup() {
+const char *getMacAddress()
+{
+  // RETORNA ENDEREÇO FÍSICO QUE O PHS DEU PARA CADA PLACA
+}
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println(F("RF24/Dakota-client"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
- 
-  // Setup and configure radio
 
+  // CONFIGURA PLACA E INICIA O RÁDIO
   radio.begin();
   radio.startListening();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // MODO DE TRANSMISSÃO
+  if (currentMode == transmitting)
+  {
+  }
+  // MODO DE ESCUTA
+  if (currentMode == listening)
+  {
+  }
+  //CLIENTE EM MODO DE ESCUTA
+  if (Serial.available())
+  {
+    if (currentMode == listening)
+    {
+      Serial.println(F("*** MUDANDO CLIENTE PARA MODO TRANSMISSÃO"));
+    }
+    else if (role == transmitting)
+    {
+      Serial.println(F("*** MUDANDO CLIENTE PARA MODO LEITURA"));
+    }
+  }
 }
--- dakota-server/dakota-server.ino
+++ dakota-server/dakota-server.ino
@@ -1,23 +1,61 @@
 #include <SPI.h>
 #include "RF24.h"
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
+/* CONFIGURAÇÃO DE HARDWARE: CONFIGURA O RÁDIO nRF24L01 EM BARRAMENTO SPI COM PINOS 7 & 8 */
+RF24 radio(7, 8);
+// DEFININDO OS MODOS QUE O NÓ PODE ASSUMIR
+typedef enum
+{
+  transmitting = 1,
+  listening
+} mode;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println(F("RF24/Dakota-client"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
- 
-  // Setup and configure radio
+mode currentMode = listening;
+const char *myAddress;
 
-  radio.begin();
-  radio.startListening();
-
+//FUNÇÃO QUE INICIA E MANTÉM A TABELA DE ENDEREÇOS DOS CLIENTES;
+void dtcp()
+{
+  Serial.println(F("RF24/Dakota-server"));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+const char *getMacAddress()
+{
+  // RETORNA ENDEREÇO FÍSICO QUE O PHS DEU PARA CADA PLACA
+}
 
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(F("RF24/Dakota-server"));
+
+  // CONFIGURA PLACA E INICIA O RÁDIO
+  radio.begin();
+  radio.startListening();
+}
+
+void loop()
+{
+
+  // MODO DE TRANSMISSÃO
+  if (currentMode == transmitting)
+  {
+  }
+  // MODO DE ESCUTA
+  if (currentMode == listening)
+  {
+  }
+  //SERVIDOR EM MODO DE ESCUTA
+  if (Serial.available())
+  {
+    if (currentMode == listening)
+    {
+      Serial.println(F("*** MUDANDO SERVIDOR PARA MODO TRANSMISSÃO"));
+    }
+    else if (role == transmitting)
+    {
+      Serial.println(F("*** MUDANDO SERVIDOR PARA MODO LEITURA"));
+    }
+  }
 }
